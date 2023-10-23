43d487a41d9df32871eeebba131693d1fd9ba1d3
Raphael Isemann <teemperor@gmail.com>
2023-10-23 10:26:25 +0200

    init

---
+++ include/FaultMacros.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stdlib.h>
+
+namespace Faults {
+inline bool isActive(const char *BugName) { return getenv(BugName) != nullptr; }
+} // namespace Faults
+
+#define FAULT_STRINGIZE_DETAIL(x) #x
+
+#define FAULT_STRINGIZE(x) FAULT_STRINGIZE_DETAIL(x)
+
+#define FAULT_GENERATE_UID                                                     \
+  "INJECTED_FAULT_" __FILE_NAME__                                              \
+  "_" FAULT_STRINGIZE(__LINE__) "_" FAULT_STRINGIZE(__COUNTER__)
+#define FAULT_IF_IS_ACTIVE if (Faults::isActive(FAULT_GENERATE_UID))
+
+#define FAULT_IS_ACTIVE_SUFFIX(SUFFIX)                                         \
+  Faults::isActive(FAULT_GENERATE_UID "_" FAULT_STRINGIZE(SUFFIX))
+
+#define FAULT_IF_IS_ACTIVE_SUFFIX(SUFFIX) if (FAULT_IS_ACTIVE_SUFFIX(SUFFIX))
+
+#define FAULT_RETURN FAULT_IF_IS_ACTIVE return;
+
+#define FAULT_RETURN_BOOL                                                      \
+  FAULT_IF_IS_ACTIVE_SUFFIX(TRUE) return true;                                 \
+  FAULT_IF_IS_ACTIVE_SUFFIX(FALSE) return false;
+
+#define FAULT_RETURN_VAL(VAL) FAULT_IF_IS_ACTIVE return (VAL);
+
+#define FAULT_RETURN_VAL2(VAL1, VAL2)                                          \
+  FAULT_IF_IS_ACTIVE_SUFFIX(OPT1) return VAL1;                                 \
+  FAULT_IF_IS_ACTIVE_SUFFIX(OPT2) return VAL2;
+
+#define FAULT_BREAK FAULT_IF_IS_ACTIVE break;
+
+#define FAULT_INT_IMPL(NUM, TYPE)                                              \
+  (FAULT_IS_ACTIVE_SUFFIX(LESS)                                                \
+       ? ((TYPE)((NUM) - (TYPE)1))                                             \
+       : (FAULT_IS_ACTIVE_SUFFIX(MORE)                                         \
+              ? ((TYPE)((NUM) + (TYPE)1))                                      \
+              : (FAULT_IS_ACTIVE_SUFFIX(INVERSE) ? ((TYPE)(~(NUM)))            \
+                                                 : ((TYPE)(NUM)))))
+#define FAULT_INT(NUM) ((decltype(NUM))FAULT_INT_IMPL(NUM, decltype(NUM)))
+
+#define FAULT_IF_COND                                                          \
+  FAULT_IS_ACTIVE_SUFFIX(FORCE_ON) || !FAULT_IS_ACTIVE_SUFFIX(FORCE_OFF) &&
---
+++ inject-faults.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+import argparse
+import glob
+import subprocess as sp
+from pathlib import Path
+
+def get_compilation_cmd(src_file):
+    paths = glob.glob('compile_commands.json', recursive=True)
+    for db in paths:
+        f = open(db, "r")
+        j = json.load(f)
+        for x in j:
+            file_path = x["file"]
+            if os.path.abspath(file_path) == src_file:
+                return x
+    raise RuntimeError("Can't find compilation cmd for " + src_file)
+
+def get_ast(info):
+    cmd = info["command"] + " -fno-color-diagnostics -Xclang -ast-dump=json "
+    res = sp.run(cmd, cwd=info["directory"], shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    return json.loads(res.stdout.decode("utf-8"))
+
+def compile_file(info):
+    try:
+        sp.check_call(info["command"], cwd=info["directory"], shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+class ReplaceData:
+    Integer = 1
+    InsertBreak = 2
+
+def get_replace_data(node, surrounding_func, in_loop):
+    # Don't replace things from included files.
+    if "loc" in node.keys():
+        if 'includedFrom' in node["loc"].keys():
+            return None
+    if "range" in node.keys():
+        if 'expansionLoc' in node["range"]["begin"].keys():
+            return None
+        
+    if "kind" in node.keys():
+        kind = node["kind"]
+        if kind == "IntegerLiteral":
+            return [ReplaceData.Integer, node]
+    return None
+
+def find_nodes_to_instrument(ast, surrounding_func = None, in_loop = False):
+    result = []
+
+    replace_data = get_replace_data(ast, surrounding_func, in_loop)
+    if replace_data:
+        result.append(replace_data)
+
+    if "kind" in ast.keys():
+        if ast["kind"] in ["CXXMethodDecl", "FunctionDecl"]:
+            surrounding_func = ast
+        if ast["kind"] in ["WhileStmt", "ForStmt", "CXXForRangeStmt"]:
+            in_loop = True
+
+    if 'inner' in ast.keys():
+        for subnode in ast["inner"]:
+            result += find_nodes_to_instrument(subnode, surrounding_func, in_loop)
+
+    return result
+
+def instrument_file(src_file):
+    src_file = os.path.realpath(src_file)
+    print("Finding compilation args...")
+    info = get_compilation_cmd(src_file)
+    print("Parsing AST...")
+    ast = get_ast(info)
+
+    if not compile_file(info):
+        print("Failed to compile initial file")
+        sys.exit(1)
+
+    print("Finding nodes to replace...")
+    nodes = find_nodes_to_instrument(ast)
+
+    for i in nodes:
+        print(i[1])
+
+instrument_file(sys.argv[1])
