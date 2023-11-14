ed64c408d610a49fe4caf5256e29fc1a97ed7942
Raphael Isemann <teemperor@gmail.com>
2023-11-14 14:33:20 +0100

    Give more unique names to macros

--- include/FaultMacros.h
+++ include/FaultMacros.h
@@ -15,45 +15,41 @@ inline bool isActive(const char *BugName) { return getenv(BugName) != nullptr; }
 #define FAULT_GENERATE_UID                                                     \
   "INJECTED_FAULT_" __FILE_NAME__                                              \
   "_" FAULT_STRINGIZE(__LINE__) "_" FAULT_STRINGIZE(__COUNTER__)
-#define FAULT_IF_IS_ACTIVE if (Faults::isActive(FAULT_GENERATE_UID))
 
-#define FAULT_IS_ACTIVE_SUFFIX(SUFFIX)                                         \
+#define FAULT_IF_IS_ACTIVE(SUFFIX)                                             \
+  if (Faults::isActive(FAULT_GENERATE_UID "_" FAULT_STRINGIZE(SUFFIX)))
+
+#define FAULT_IS_ACTIVE(SUFFIX)                                                \
   Faults::isActive(FAULT_GENERATE_UID "_" FAULT_STRINGIZE(SUFFIX))
 
-#define FAULT_IF_IS_ACTIVE_SUFFIX(SUFFIX) if (FAULT_IS_ACTIVE_SUFFIX(SUFFIX))
+#define FAULT_IF_IS_ACTIVE_SUFFIX(SUFFIX) if (FAULT_IS_ACTIVE(SUFFIX))
 
-#define FAULT_RETURN FAULT_IF_IS_ACTIVE return;
+#define FAULT_RETURN FAULT_IF_IS_ACTIVE(RETURN_VOID) return;
 
-#define FAULT_RETURN_INT FAULT_IF_IS_ACTIVE_SUFFIX(TRUE) return 0; 
+#define FAULT_RETURN_INT FAULT_IF_IS_ACTIVE_SUFFIX(RETURN_ZERO) return 0;
 
-#define FAULT_CONDITIONAL if (! FAULT_IS_ACTIVE_SUFFIX(CONDITIONAL))
+#define FAULT_CONDITIONAL if (!FAULT_IS_ACTIVE(CONDITIONAL_SKIP))
 
 #define FAULT_RETURN_BOOL                                                      \
-  FAULT_IF_IS_ACTIVE_SUFFIX(TRUE) return true;                                 \
-  FAULT_IF_IS_ACTIVE_SUFFIX(FALSE) return false;
+  FAULT_IF_IS_ACTIVE(RETURN_TRUE) return true;                                 \
+  FAULT_IF_IS_ACTIVE(RETURN_FALSE) return false;
 
-#define FAULT_RETURN_VAL(VAL) FAULT_IF_IS_ACTIVE return (VAL);
+#define FAULT_RETURN_VAL(VAL) FAULT_IF_IS_ACTIVE(RETURN_VAL) return (VAL);
 
-#define FAULT_RETURN_VAL2(VAL1, VAL2)                                          \
-  FAULT_IF_IS_ACTIVE_SUFFIX(OPT1) return VAL1;                                 \
-  FAULT_IF_IS_ACTIVE_SUFFIX(OPT2) return VAL2;
-
-#define FAULT_BREAK FAULT_IF_IS_ACTIVE break;
+#define FAULT_BREAK FAULT_IF_IS_ACTIVE(BREAK) break;
 
 #define FAULT_INT_IMPL(NUM, TYPE)                                              \
-  (FAULT_IS_ACTIVE_SUFFIX(LESS)                                                \
+  (FAULT_IS_ACTIVE(INT_LESS)                                                   \
        ? ((TYPE)((NUM) - (TYPE)1))                                             \
-       : (FAULT_IS_ACTIVE_SUFFIX(MORE)                                         \
+       : (FAULT_IS_ACTIVE(INT_MORE)                                            \
               ? ((TYPE)((NUM) + (TYPE)1))                                      \
-              : (FAULT_IS_ACTIVE_SUFFIX(INVERSE) ? ((TYPE)(~(NUM)))            \
-                                                 : ((TYPE)(NUM)))))
+              : (FAULT_IS_ACTIVE(INT_INVERSE) ? ((TYPE)(~(NUM)))               \
+                                              : ((TYPE)(NUM)))))
 #define FAULT_INT(NUM) ((decltype(NUM))FAULT_INT_IMPL(NUM, decltype(NUM)))
 
 #define FAULT_BOOL_IMPL(NUM, TYPE)                                             \
-  (FAULT_IS_ACTIVE_SUFFIX(FLIP_BOOL)                                           \
-       ? ((TYPE)(!(NUM)))                                                      \
-       : ((TYPE)(NUM)))
+  (FAULT_IS_ACTIVE(BOOL_FLIP) ? ((TYPE)(!(NUM))) : ((TYPE)(NUM)))
 #define FAULT_BOOL(NUM) ((decltype(NUM))FAULT_BOOL_IMPL(NUM, decltype(NUM)))
 
 #define FAULT_IF_COND                                                          \
-  FAULT_IS_ACTIVE_SUFFIX(FORCE_ON) || !FAULT_IS_ACTIVE_SUFFIX(FORCE_OFF) &&
+  FAULT_IS_ACTIVE(FORCE_ENABLE_COND) || !FAULT_IS_ACTIVE(FORCE_DISABLE_COND) &&
